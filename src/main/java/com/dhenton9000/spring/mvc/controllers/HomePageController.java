022ad66b561ddde374a02609b6be51ddac5fcd2f
donhenton <donhenton@gmail.com>
2016-02-16 13:41:45 -0600

    setting for development 3

--- src/main/java/com/dhenton9000/spring/mvc/controllers/HomePageController.java
+++ src/main/java/com/dhenton9000/spring/mvc/controllers/HomePageController.java
@@ -2,9 +2,7 @@ package com.dhenton9000.spring.mvc.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 import org.apache.log4j.*;
-import com.fasterxml.jackson.databind.JsonMappingException;
 
 
 /**
